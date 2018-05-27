674351ea0fbe0b00935befa376aebdfe4b515af3
dannyrb <danny.ri.brown@gmail.com>
2018-05-26 23:30:07 -0400

    Hammering out some basic copy

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -6,11 +6,33 @@
 			<v-container fluid>
 				<h1>v-cache examples</h1>
 
+				The custom Vue Directive, v-cache, can be attached to any Vue component. The only required parameter is a unique string that can
+				be used as a "caching key". In example:<br />
+				<br />
+				<code>&lt;my-custom-form-component v-cache="'my-custom-cache-key'" /&gt;</code><br />
+				<br />
+				When a component uses the v-cache directive, it gains the ability to cache, restore, or clear the state of:<br />
+				<br />
+				<code style="white-space: normal">
+				data: {
+					form: {
+						// Cache, restore, or clear this state
+					}
+				}</code><br />
+				<br />
+				By emitting these events to trigger the directive's behavior:<br />
+				<br />
+<code>this.$emit("cache");
+this.$emit("cache-restore");
+this.$emit("cache-clear");
+</code>
+
 				<v-layout row wrap>
 
 					<!-- BASIC -->
 					<v-flex xs12 md6>
-						<h2>Basic Example</h2>		
+						<h2>Basic Example</h2>
+						Try reloading the page after typing in this form! You can even close the tab (or your browser entirely).
 						<v-card style="padding: 30px 45px;">
 							<basic-example v-cache="'basic-form-cache-key'" />
 						</v-card>
